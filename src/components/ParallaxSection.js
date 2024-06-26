84ef757bd9d4770bca5f174276ca35f1de41e0a1
rattle90 <yuapedroza@gmail.com>
2024-06-26 18:13:39 +0700

    Y

--- src/components/ParallaxSection.js
+++ src/components/ParallaxSection.js
@@ -4,7 +4,15 @@ import "./ParallaxSection.css";
 const ParallaxSection = () => {
   return (
     <div className="parallax">
-      <h1>Happy Birthday</h1>
+      <h1 className="heart-container">
+        Happy Birthday
+        <span className="heart heart1"></span>
+        <span className="heart heart2"></span>
+        <span className="heart heart3"></span>
+        <span className="heart heart4"></span>
+        <span className="heart heart5"></span>
+        <span className="heart heart6"></span>
+      </h1>
     </div>
   );
 };
