6800884623c4b883d4ec7f88140e260198ac7180
rattle90 <yuapedroza@gmail.com>
2024-07-01 00:18:34 +0700

    6

--- src/components/ParallaxSection.js
+++ src/components/ParallaxSection.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./ParallaxSection.css";
 
 const ParallaxSection = () => {
+  useEffect(() => {
+    const cursorLight = document.querySelector(".cursor-light");
+
+    const moveCursorLight = (e) => {
+      cursorLight.style.transform = `translate(${e.clientX - 50}px, ${e.clientY - 50}px)`;
+    };
+
+    document.addEventListener("mousemove", moveCursorLight);
+
+    return () => {
+      document.removeEventListener("mousemove", moveCursorLight);
+    };
+  }, []);
+
   return (
     <div className="parallax">
       <div className="text-container heart-container">
@@ -13,6 +27,7 @@ const ParallaxSection = () => {
         <span className="heart heart4"></span>
         <span className="heart heart5"></span>
         <span className="heart heart6"></span>
+        <div className="cursor-light"></div>
       </div>
     </div>
   );
